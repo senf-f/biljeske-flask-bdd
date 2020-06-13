882eb474bdbd3cb25d2aac4e07464cb572b6f1aa
Mate <mate.mrse@gmail.com>
2020-06-13 14:47:49 +0200

    added inital version of the first scenario

--- src/main/java/stepdefs/Steps.java
+++ src/main/java/stepdefs/Steps.java
@@ -5,13 +5,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.Arrays;
+
 public class Steps {
 
     WebDriver driver;
+    int brojRedova;
+    int noviBrojRedova;
 
     @Before
     public void setUp(){
@@ -30,12 +35,23 @@ public class Steps {
 
     @When("user adds a note")
     public void userAddsANote() {
-        System.out.println("User adds a note");
+        //TODO: dodati random id po kojem ćemo kasnije brisati bilješku
+        brojRedova = driver.findElements(By.cssSelector("table tr")).size();
+
+        driver.findElement(By.id("category")).sendKeys("TEST kategorija");
+        driver.findElement(By.id("note")).sendKeys("TEST bilješka");
+        driver.findElement(By.cssSelector("input[type='submit']")).click();
+
+        noviBrojRedova = driver.findElements(By.cssSelector("table tr")).size();
+        Assert.assertEquals(noviBrojRedova, brojRedova + 1);
+        System.out.println("Bilješka uspješno dodana!");
     }
 
     @Then("note is visible in the list")
     public void noteIsVisibleInTheList() {
-        System.out.println("note is visible in the list");
+        String posljednjiRed = driver.findElement(By.cssSelector("table tr:last-child")).getText();
+        System.out.println(Arrays.toString(posljednjiRed.split(" ")));
+        //TODO: dodati provjeru datuma i vremena
     }
 
     @When("user edits a note")
