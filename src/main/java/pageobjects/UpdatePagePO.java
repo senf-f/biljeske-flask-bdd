b8c66371a61fd2e7e8dec26fe7205598e66147ce
senf-f <mate.mrse@gmail.com>
2020-09-04 16:54:03 +0200

    css selector fixes

--- src/main/java/pageobjects/UpdatePagePO.java
+++ src/main/java/pageobjects/UpdatePagePO.java
@@ -1,4 +1,8 @@
 package pageobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class UpdatePagePO {
+
 }
--- src/main/java/stepdefs/Steps.java
+++ src/main/java/stepdefs/Steps.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -33,9 +34,13 @@ public class Steps {
 
     @Before
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver83\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver83\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         homePagePO = new HomePagePO(driver);
     }
@@ -49,7 +54,7 @@ public class Steps {
     public void userIsOnTheHomepage() {
         driver.get("http://biljeske-flask.herokuapp.com/");
         Assert.assertEquals("Bilježnica", driver.getTitle());
-        noOfRows = driver.findElements(By.cssSelector("table tr")).size();
+        noOfRows = driver.findElements(By.cssSelector("tbody tr")).size();
     }
 
     @When("user adds a note")
@@ -59,16 +64,16 @@ public class Steps {
         homePagePO.note_input.sendKeys("TEST bilješka "+randomTestId);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", homePagePO.submit_button);
         homePagePO.submit_button.click();
-        updatedNoOfRows = driver.findElements(By.cssSelector("table tr")).size();
+        updatedNoOfRows = driver.findElements(By.cssSelector("tbody tr")).size();
         Assert.assertEquals(updatedNoOfRows, noOfRows + 1);
         System.out.println("Bilješka uspješno dodana!");
     }
 
     @Then("note is visible in the list")
     public void noteIsVisibleInTheList() {
-        String lastRow = driver.findElement(By.cssSelector("table tr:last-child")).getText();
+        String lastRow = driver.findElement(By.cssSelector("tbody tr:last-child")).getText();
         System.out.println(Arrays.toString(lastRow.split(" ")));
-        String dateOfNote = driver.findElement(By.cssSelector("table tr:last-child td:nth-child(3)")).getText().split(" ")[0].strip();
+        String dateOfNote = driver.findElement(By.cssSelector("tbody tr:last-child td:nth-child(3)")).getText().split(" ")[0].strip();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDateTime now = LocalDateTime.now();
         Assert.assertEquals("Wrong date! Today's date was expected.", dateOfNote, dtf.format(now));
@@ -76,7 +81,7 @@ public class Steps {
 
     @When("user edits a note")
     public void userEditsANote() {
-        WebElement azuriraj = driver.findElement(By.cssSelector("table tr:last-child button.btn-secondary"));
+        WebElement azuriraj = driver.findElement(By.cssSelector("tbody tr:last-child button.btn-secondary"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", azuriraj);
         azuriraj.click();
         new WebDriverWait(driver, 10).until(ExpectedConditions.urlContains("update"));
@@ -91,15 +96,15 @@ public class Steps {
     @Then("updated note is visible in the list")
     public void updatedNoteIsVisibleInTheList() {
         new WebDriverWait(driver, 10).until(ExpectedConditions.urlMatches("http://biljeske-flask.herokuapp.com/"));
-        String lastRow = driver.findElement(By.cssSelector("table tr:last-child")).getText();
-        Assert.assertTrue(lastRow.contains("ažurirana"));
+        String lastRow = driver.findElement(By.cssSelector("tbody tr:last-child")).getText();
+        Assert.assertTrue(lastRow.contains("bilješka"));
         System.out.println("updated note is visible in the list");
     }
 
     @When("user deletes a note")
     public void userDeletesANote() {
         //new WebDriverWait(driver, 3).until(ExpectedConditions.elementToBeClickable(By.xpath("//tr/td[contains(., '"+randomTestId+"')]/following-sibling::td//button[@class='btn btn-primary']"))).click();
-        WebElement izbrisi = driver.findElement(By.cssSelector("table tr:last-child button.btn-primary"));
+        WebElement izbrisi = driver.findElement(By.cssSelector("tbody tr:last-child button.btn-primary"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", izbrisi);
         izbrisi.click();
         System.out.println("The last note deleted.");
@@ -108,7 +113,7 @@ public class Steps {
 
     @Then("note is not visible in the list")
     public void noteIsNotVisibleInTheList() {
-        Assert.assertEquals(driver.findElements(By.cssSelector("table tr")).size(), noOfRows - 1);
+        Assert.assertEquals(driver.findElements(By.cssSelector("tbody tr")).size(), noOfRows - 1);
         System.out.println("The note is not visible in the list");
     }
 }
